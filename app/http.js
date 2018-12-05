5c18b5e4a391dea0277d232a51e25158a81c048e
Fredrik Eklund <fredrik.eklund@attentec.se>
2018-12-05 13:47:50 +0100

    add service to listen for http requests

---
+++ app/http.js
@@ -0,0 +1,71 @@
+/* global console, chrome WSC, _ */
+
+// ----- handle http requests ------
+
+class RequestHandler extends WSC.BaseHandler {
+    constructor() {
+        super();
+        this.disk=null;
+        chrome.runtime.getPackageDirectoryEntry(function(entry) {
+            this.disk=entry;
+        }.bind(this));
+    }
+}
+
+const RequestHandlerPrototype={
+    get: function(path) {
+        let message='stuff';
+        try {
+            const req=this.request;
+            message=JSON.stringify({
+                arguments: req.arguments,
+                headers: req.headers,
+                method: req.method,
+                path: req.path,
+            }, null, 4);
+            this.respond(message, 200);
+        } catch (error) {
+            this.respond(error, 500);
+        }
+    },
+    put: function(path) {
+        try {
+            this.respond('ok', 200);
+        } catch (error) {
+            this.respond('error', 500);
+        }
+    },
+    post: function(path) {
+        try {
+            this.respond('ok', 200);
+        } catch (error) {
+            this.respond(error, 500);
+        }
+    },
+    onReadFile: function(evt) {
+        if (evt.error) {
+            this.respond('disk access error', 500);
+        } else {
+            this.respond(evt, 200);
+        }
+    },
+
+    respond: function(message, status) {
+        console.log(status, message);
+        this.write(message, status);
+    },
+};
+RequestHandlerPrototype.respond.bind(RequestHandlerPrototype);
+
+_.extend(RequestHandler.prototype, RequestHandlerPrototype, WSC.BaseHandler.prototype);
+
+// ----- start server -----
+
+const options={
+    handlers: [
+        ['.*', RequestHandler],
+    ],
+};
+const app=new WSC.WebApplication(options);
+app.start();
+console.log(app);

7cc1007ee88f9e8db7cb122da1263e1ce321cff8
Fredrik Eklund <fredrik.eklund@attentec.se>
2018-12-06 16:59:04 +0100

    mock a GUI for messages between app and extension

--- app/ui/index.js
+++ app/ui/index.js
@@ -1,34 +1,77 @@
 /* global document, chrome, window */
 (function(context) {
-    const whitelistedId='cndppbofippbaceojanmcndelhdknaag';
+    const EXTENSION_ID='cndppbofippbaceojanmcndelhdknaag';
 
     const logField=document.getElementById('log');
     const sendText=document.getElementById('sendText');
-    const sendId={ value: whitelistedId };
-    const send=document.getElementById('send');
+    const sendMessageButton=document.getElementById('send');
 
-    send.addEventListener('click', function() {
-        appendLog('sending to '+sendId.value);
-        if (sendText.value===''||Number.isNaN(Number(sendText.value))) {
-            chrome.runtime.sendMessage(
-                sendId.value,
-                { myCustomMessage: sendText.value, cmd: 'notification' },
-                function(response) {
-                    appendLog('response: '+JSON.stringify(response, null, 4));
-                });
+    const prevTabButton=document.getElementById('prevTab');
+    const nextTabButton=document.getElementById('nextTab');
+    const focusTabButton=document.getElementById('focusTab');
+    const focusTabIndex=document.getElementById('focusTabIndex');
+    const focusWindowButton=document.getElementById('focusWindow');
+    const focusWindowIndex=document.getElementById('focusWindowIndex');
+    const getAllWindows=document.getElementById('getAllWindows');
+
+    sendMessageButton.addEventListener('click', function() {
+        chrome.runtime.sendMessage(
+            EXTENSION_ID,
+            { message: sendText.value, cmd: 'notification', method: 'POST' },
+            function(response) {
+                appendLog('response: '+JSON.stringify(response));
+            });
+    });
+
+    focusWindowButton.addEventListener('click', function() {
+        const index = focusWindowIndex.value;
+        if (index===''||Number.isNaN(Number(index))) {
+            return;
         } else {
             chrome.runtime.sendMessage(
-                sendId.value,
-                { value: sendText.value, cmd: 'focusTabByIndex', method: 'POST' },
+                EXTENSION_ID,
+                { value: Number(index), cmd: 'focusWindowByIndex', method: 'POST' },
                 function(response) {
-                    appendLog('response: '+JSON.stringify(response, null, 4));
+                    appendLog('response: '+JSON.stringify(response));
                 });
         }
     });
 
+    focusTabButton.addEventListener('click', function() {
+        const index = focusTabIndex.value;
+        if (index===''||Number.isNaN(Number(index))) {
+            return;
+        } else {
+            chrome.runtime.sendMessage(
+                EXTENSION_ID,
+                { value: Number(index), cmd: 'focusTabByIndex', method: 'POST' },
+                function(response) {
+                    appendLog('response: '+JSON.stringify(response));
+                });
+        }
+    });
+
+    prevTabButton.addEventListener('click', function() {
+        chrome.runtime.sendMessage(
+            EXTENSION_ID,
+            { cmd: 'focusPreviousTab', method: 'POST' },
+            function(response) {
+                appendLog('response: '+JSON.stringify(response));
+            });
+    });
+
+    nextTabButton.addEventListener('click', function() {
+        chrome.runtime.sendMessage(
+            EXTENSION_ID,
+            { cmd: 'focusNextTab', method: 'POST' },
+            function(response) {
+                appendLog('response: '+JSON.stringify(response));
+            });
+    });
+
     chrome.runtime.onMessageExternal.addListener(
         function(request, sender, sendResponse) {
-            if (sender.id===whitelistedId) {
+            if (sender.id===EXTENSION_ID) {
                 appendLog('from '+sender.id+': '+request.myCustomMessage);
                 sendResponse({ 'status': 200 });
             } else if (request.myCustomMessage) {
@@ -38,6 +81,15 @@
                 sendResponse({ 'status': 500 });
             }
         });
+
+    getAllWindows.addEventListener('click', function() {
+        chrome.runtime.sendMessage(
+            EXTENSION_ID,
+            { cmd: 'getAllWindows', method: 'GET' },
+            function(response) {
+                appendLog('response: '+JSON.stringify(response, null, 4));
+            });
+    });
 
     const appendLog=function(message) {
         logField.innerText+='\n'+message;
